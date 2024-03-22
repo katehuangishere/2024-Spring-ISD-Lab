74f05530effb27e05250ac637f5daec56207935e
Kate Huang <102621614+katehuangishere@users.noreply.github.com>
2024-03-21 23:26:00 -0400

    Add files via upload

---
+++ lab1 - hello world.cpp
@@ -0,0 +1,11 @@
+﻿// lab1 - hello world.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "std_lib_facilities.h"
+
+int main()
+{
+	cout << "Hello World!\n";
+
+	return 0;
+}
